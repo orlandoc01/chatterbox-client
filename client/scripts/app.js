fd7e00f03116ae5e8f0a1ba4146c8fae19b3764c
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-01-25 11:32:59 -0800

    refactors code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,38 +1,34 @@
 // YOUR CODE HERE:
 
 var app = {
-	server: 'https://api.parse.com/1/classes/chatterbox'
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  init: function() { 
+  },
+  send: function(message) {
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/JSON',
+      sucess: function(data) {
+        console.log("Message sent to chatterbox")
+      },
+      error: function(data) {
+        console.error("Failure: Message not sent.")
+      }
+    });
+  },
+  fetch: function() {
+  	$.ajax(this.server, {
+      sucess: function(data) {
+      	console.log("Message received was " + data);
+      	return data;
+      },
+      error: function(data) {
+      	console.error("Failure: message not received");
+      }	
+    });
+  }
 };
 
-app.init = function() {
-
-}
-
-app.send = function(message) {
-	$.ajax({
-		 url: this.server,
-		 type: 'POST',
-		 data: JSON.stringify(message),
-		 contentType: 'application/JSON',
-		 sucess: function(data) {
-		 	console.log("Message sent to chatterbox")
-		 },
-		 error: function(data) {
-		 	console.error("Failure: Message not sent.")
-		 }
-	});
-
-}
-
-app.fetch = function() {
-	$.ajax(this.server, {
-		sucess: function(data) {
-			console.log("Message received was " + data);
-			return data;
-		},
-		error: function(data) {
-			console.error("Failure: message not received");
-		}
-		
-	});
-}
+ 
