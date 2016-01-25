97c4b1df5d6f921e8206ed0c30b45e11df77303f
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-01-25 11:06:22 -0800

    Initializes app, creates init and send methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,23 @@
 // YOUR CODE HERE:
 
+var app = {};
+
+app.init = function() {
+
+}
+
+app.send = function(message) {
+	$.ajax({
+		 url: 'https://api.parse.com/1/classes/chatterbox',
+		 type: 'POST',
+		 data: JSON.stringify(message),
+		 contentType: 'application/JSON',
+		 sucess: function(data) {
+		 	console.log("Message sent to chatterbox")
+		 },
+		 error: function(data) {
+		 	console.error("Failure: Message not sent.")
+		 }
+	});
+
+}
